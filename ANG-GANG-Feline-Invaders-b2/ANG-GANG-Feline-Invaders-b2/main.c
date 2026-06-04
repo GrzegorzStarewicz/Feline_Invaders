085e27e0c8875eee85788d879f544daee155d862
GrzegorzStarewicz <stargraj@gmail.com>
2026-06-04 16:47:45 +0200

    beta2 seg7 score zycie

---
+++ ANG-GANG-Feline-Invaders-b2/ANG-GANG-Feline-Invaders-b2/main.c
@@ -0,0 +1,525 @@
+/*
+ * ANG-GANG-Feline-Invaders-b2.c
+ *
+ * Created: 03.06.2026 11:19:22
+ * 
+ * Author : £ka
+ */ 
+
+#include <avr/io.h>
+#define F_CPU 11059200UL
+#include <util/delay.h>
+#include <avr/interrupt.h>
+#include <stdlib.h>
+
+// LCD
+#define magistralaDDR DDRC
+#define magistrala PORTC
+#define sterRSDDR DDRB
+#define sterRS PORTB
+#define sterEDDR DDRD
+#define sterE PORTD
+#define sterRW PORTD
+#define RS 2 //portB
+#define E 6  //portD
+#define RW 7 //portD
+
+// znaki
+#define znGracz '>'
+#define znWrog 'X'
+#define znPocisk '='
+#define znNic ' '
+#define znOgon '-'
+#define znOgonKoniec '&'
+
+// przyciski
+#define przyciski PINB
+// nie moze byc na 2 bo to jest RS
+#define przlewoprawo 0 //PORTB
+#define przstrzel 1    //PORTB
+// ani 3 4 5 6 bo to seg7
+uint8_t zmlewoprawo = 0;
+uint8_t zmstrzel = 0;
+
+//pozycje
+uint16_t wrogowieA = 0;
+uint16_t wrogowieB = 0;
+uint16_t pociskiA = 0;
+uint16_t pociskiB = 0;
+uint16_t eliminacja = 0;
+
+//boole gracza
+uint8_t pozycja = 0;
+uint8_t strzel = 0;
+
+//bool T0 przyciski
+uint8_t sprawdzprz = 0;
+//bool T1 clock
+uint8_t cykl = 0;
+
+//czestosc wrogow 1/x
+uint8_t trudnosc = 4;
+//szybkosc timera 1 gry 10500 - szybkie, 5000 - bardzo szybkie, 20000 - wolne
+uint16_t szybkosc_gry = 14500;
+#define przyspieszanie 32
+
+//dane gry
+uint8_t zycie = 9;
+uint16_t score = 0;
+
+//seg7 na 3 4 5 6 ???
+uint8_t dane[] = {1,2,3,4};
+#define segmentyDDR DDRA //portA I/O
+#define segmenty PORTA //diody na porcie A
+#define wyborDDR DDRB
+#define wybor PORTB	
+uint8_t seg7[] = { 0xc0,0xf9,0xa4,0xb0,0x99,0x92,0x82,0xd8,0x80,0x90 };
+
+//rand something something
+void ADC_init() {
+	ADMUX = (1 << REFS0); // Use AVCC as reference
+	ADCSRA = (1 << ADEN) | (1 << ADPS2) | (1 << ADPS1); // Enable ADC, prescaler 64
+}
+uint16_t get_entropy() {
+	uint16_t seed = 0;
+	for (int i = 0; i < 16; i++) {
+		ADCSRA |= (1 << ADSC); // Start ADC conversion
+		while (ADCSRA & (1 << ADSC)); // Wait for conversion to finish
+		seed = (seed << 1) | (ADC & 0x01); // Extract the least significant bit
+	}
+	return seed;
+}
+
+
+void LCD_zapis(uint8_t dane)
+{
+	
+	// zbocze opadajace (?)
+	magistrala = dane;
+	sterE |= (1<<E); //E = 1
+	_delay_us(20);
+	sterE &= ~(1<<E); //E = 0
+	_delay_us(50);
+	//_delay_ms(2);
+}
+
+void LCD_init(){
+	// DDR
+	magistralaDDR = 0xff;
+	sterRSDDR |= (1<<RS); //wyjscie
+	sterEDDR |= (1<<E)|(1<<RW);
+	
+	// inicjalizacja wyswietlacza LCD do pracy
+	_delay_ms(5);
+	sterRS &= ~(1<<RS); // bedziemy podawac instrukcje
+	sterRW &= ~(1<<RW);
+	
+	// magistrala 8-bitowa
+	// function set 00111000 - 001DNFXX
+	// == 0x38
+	LCD_zapis(0x38);
+	
+	// inkrementacja znaku
+	// entry mode set 00000110 - 000001IS
+	// == 0x06
+	LCD_zapis(0x06);
+	
+	// wlaczenie LCD
+	// display ON/OFF 00001111 -
+	// == 0x0f / 0x0c bez kursora
+	LCD_zapis(0x0c);
+	
+	// czyszczenie - trzeba zdelayowac
+	LCD_zapis(0x01);
+	_delay_ms(2);
+}
+
+void LCD_znak(uint8_t zn)
+{
+	static uint8_t j = 0;
+	//zn = zn + '0';
+	if(zn == '\b') {
+		if(j != 0) j--;
+		else j = 31;
+		
+		uint8_t pozycja;
+		
+		sterRS &= ~(1<<RS);
+		if(j < 16) pozycja = 0x80+j;
+		else pozycja = 0xc0+j-16;
+		LCD_zapis(pozycja);
+		
+		sterRS |= (1<<RS);
+		LCD_zapis(' ');
+		
+		sterRS &= ~(1<<RS);
+		LCD_zapis(pozycja);
+	}
+	else
+	{
+		if(j==16)
+		{
+			sterRS &= ~(1<<RS); //wybor gdzie piszemy
+			LCD_zapis(0xc0);
+		}
+		if(j==32)
+		{
+			sterRS &= ~(1<<RS); //wybor gdzie piszemy
+			LCD_zapis(0x80);
+			j = 0;
+		}
+		
+		sterRS |= (1<<RS);
+		LCD_zapis(zn+j);
+		j++;
+	}
+}
+
+void LCD_zpA(char znak, uint8_t poz)
+{
+	sterRS &= ~(1<<RS);
+	LCD_zapis(0x80+poz);
+	sterRS |= (1<<RS);
+	LCD_zapis(znak);
+}
+
+void LCD_zpB(char znak, uint8_t poz)
+{
+	sterRS &= ~(1<<RS);
+	LCD_zapis(0xc0+poz);
+	sterRS |= (1<<RS);
+	LCD_zapis(znak);
+}
+
+void Napis(uint8_t *tekst)
+{
+	uint8_t i=0;
+	
+	sterRS &= ~(1<<RS); // wybor gdzie piszemy
+	LCD_zapis(0x80);
+	
+	sterRS |= (1<<RS);
+	while(tekst[i])
+	{
+		LCD_zapis(tekst[i++]);
+		if(i==16)
+		{
+			sterRS &= ~(1<<RS); //wybor gdzie piszemy
+			LCD_zapis(0xc0);
+			sterRS |= (1<<RS);
+		}
+	}
+}
+
+// timer cyklu 
+void T1_init(){
+	TCCR1B = (1<<WGM12)|(1<<CS12); //WGM12 - tryb CTC, CS12 - clk/256
+	//TCCR1B = (1<<WGM12)|(1<<CS10);
+	TIMSK |= (1<<OCIE1A); //w TIMSKu zgadzamy sie na to przerwanie
+	OCR1A = szybkosc_gry;
+	//OCR1A = szybkosc_gry; //prescaler
+	
+	sei();
+}
+
+// timer przyciskow
+void T0_init(){
+	TCCR0 = (1<<CS01)|(1<<CS00); //CS02,CS00 - clk/1024, WGM01 - tryb CTC
+	TIMSK |= (1<<TOIE0); //przerwanie od przepelnienia t0
+	//OCR0 = 0xff; 
+}
+
+void Przyciski_init(){
+	sterRSDDR &= ~((1<<przlewoprawo)|(1<<przstrzel)); //przycisk jako wejscie
+	sterRS |= (1<<przlewoprawo)|(1<<przstrzel); //rezystor polaryzujacy na wejsciu przycisk
+}
+
+// timer cyklu
+ISR(TIMER1_COMPA_vect)
+{
+	cykl = 1;
+}
+
+// timer przyciskow
+ISR(TIMER0_OVF_vect)
+{
+	sprawdzprz = 1;
+}
+
+void sprawdz_przyciski(){
+	// przycisk lewo prawo
+	if(!(przyciski & (1<<przlewoprawo)))
+	{
+		switch(zmlewoprawo)
+		{
+			case 0:
+			zmlewoprawo = 1;
+			break;
+			case 1:
+			zmlewoprawo = 2;
+			break;
+		}
+	}
+	else
+	{
+		switch(zmlewoprawo)
+		{
+			case 3:
+			zmlewoprawo = 4;
+			break;
+			case 4:
+			zmlewoprawo = 0;
+			break;
+		}
+	}
+	
+	// przycisk lewo prawo
+	if(!(przyciski & (1<<przstrzel)))
+	{
+		switch(zmstrzel)
+		{
+			case 0:
+			zmstrzel = 1;
+			break;
+			case 1:
+			zmstrzel = 2;
+			break;
+		}
+	}
+	else
+	{
+		switch(zmstrzel)
+		{
+			case 3:
+			zmstrzel = 4;
+			break;
+			case 4:
+			zmstrzel = 5; //na 5!
+			break;
+		}
+	}
+}
+
+void check_i_eliminacja()
+{
+	// eliminacja A
+	eliminacja = wrogowieA & pociskiA;
+	wrogowieA &= ~eliminacja;
+	pociskiA &= ~eliminacja;
+	
+	// score na podstawie liczby 1 w eliminacji
+	uint8_t count = 0;
+	for (count = 0; eliminacja; count++) {
+		eliminacja &= eliminacja - 1;
+	}
+	score += count;
+	
+	// eliminacja B
+	eliminacja = wrogowieB & pociskiB;
+	wrogowieB &= ~eliminacja;
+	pociskiB &= ~eliminacja;
+	
+	// score na podstawie liczby 1 w eliminacji
+	for (count = 0; eliminacja; count++) {
+		eliminacja &= eliminacja - 1;
+	}
+	score += count;
+}
+
+void ruch()
+{	
+	
+	static uint8_t co2 = 0;
+	if(co2){
+		wrogowieA >>= 1;
+		wrogowieB >>= 1;
+		
+		// spawnowanie wrogow
+		wrogowieA |= (rand()%trudnosc==0)<<15;
+		wrogowieB |= (rand()%trudnosc==0)<<15;
+		
+		check_i_eliminacja();
+	}
+	co2^=1;
+	
+	// zycie za pociski
+	if(pociskiA & (1<<15)) zycie--;
+	if(pociskiB & (1<<15)) zycie--;
+	
+	// zycie za wrogow
+	if(wrogowieA & 1) {zycie--; wrogowieA &= ~1;}
+	if(wrogowieB & 1) {zycie--; wrogowieB &= ~1;}
+	
+	pociskiA <<= 1;
+	pociskiB <<= 1;
+	
+	// spawnowanie pociskow
+	if(strzel == 1)
+	{
+		switch (pozycja)
+		{
+			case 0: pociskiA |= 2;
+					break;
+			case 1: pociskiB |= 2;
+					break;
+		}
+	}
+	
+	check_i_eliminacja();
+}
+
+void LCD_ekran_odswiez()
+{
+	uint8_t ogonA = 0;
+	uint8_t ogonB = 0;
+	
+	for (uint8_t i = 1; i < 16; i++)
+	{
+		// uzupelnianie linii A
+		if(wrogowieA & 1<<i){
+			if(ogonA++==0)
+			LCD_zpA(znWrog,i);
+			else
+			LCD_zpA(znOgon,i);
+		} else {
+			ogonA = 0;
+			if(pociskiA & 1<<i){
+				LCD_zpA(znPocisk,i);
+			} else
+			LCD_zpA(znNic,i);
+		}
+		
+		
+		// uzupelnianie linii B
+		if(wrogowieB & 1<<i){
+			if(ogonB++==0)
+			LCD_zpB(znWrog,i);
+			else
+			LCD_zpB(znOgon,i);
+			} else {
+			ogonB = 0;
+			if(pociskiB & 1<<i){
+				LCD_zpB(znPocisk,i);
+			} else
+			LCD_zpB(znNic,i);
+		}
+	}
+}
+
+void seg7_wyswietl()
+{
+	static uint8_t i = 3;
+	//wylacz poprzednie
+	wybor |= (1<<i);
+	
+	if(i++==6) i = 3;
+	dane[0] = zycie;
+	
+	dane[3]=score%10;
+	dane[2]=(score/10)%10;
+	dane[1]=(score/100)%10;
+	
+	segmenty = seg7[dane[i-3]];
+	
+	//wlacz nowe
+	wybor &= ~(1<<i);
+}
+
+void seg7_init()
+{
+	wyborDDR |= (1<<3)|(1<<4)|(1<<5)|(1<<6);
+	segmentyDDR |= 0xff;
+}
+
+int main(void)
+{	
+	
+	ADC_init();
+	// seed dla randa
+	srand(get_entropy());
+	
+	Przyciski_init();
+	LCD_init();
+	T0_init();
+	T1_init();
+	
+	seg7_init();
+	
+	//przykladowe rozlozenie
+	//pociskiB = 1;
+	wrogowieA = 1<<15;
+	//pociskiA  = 5;
+	wrogowieB = 1<<15;
+	
+	// pierwsza pozycja gracza
+	LCD_zpA(znGracz,0);
+	LCD_zpB(znNic,0);
+	
+    while (1) 
+    {
+		//T1 cykl
+		if(cykl == 1){
+		ruch();
+		LCD_ekran_odswiez();
+		
+		if(szybkosc_gry > 12000) szybkosc_gry -= przyspieszanie*2;
+		else if(szybkosc_gry > 10000) szybkosc_gry -= przyspieszanie;
+		else if(szybkosc_gry > 90000) szybkosc_gry -= przyspieszanie/2;
+		else if(szybkosc_gry > 8000) szybkosc_gry -= przyspieszanie/4;
+		else if(szybkosc_gry > 6000) szybkosc_gry -= przyspieszanie/8;
+		else if(szybkosc_gry > 4000) szybkosc_gry -= przyspieszanie/16;
+		else if(szybkosc_gry > 2000) szybkosc_gry -= przyspieszanie/32;
+		OCR1A = szybkosc_gry;
+		
+		cykl = 0;
+		}
+		
+		//przycisk lewo prawo
+		if(zmlewoprawo == 2){
+		switch(pozycja){
+			case 0: pozycja = 1;
+					LCD_zpA(znNic,0);
+					LCD_zpB(znGracz,0);
+					break;
+			case 1: pozycja = 0;
+					LCD_zpA(znGracz,0);
+					LCD_zpB(znNic,0);
+					break;
+		}
+		zmlewoprawo = 3;	
+		}
+		
+		//przycisk strzel
+		if (zmstrzel == 2) //start
+		{
+			switch (pozycja) //pierwszy strzal
+			{
+				case 0: pociskiA |= 2;
+						LCD_zpA(znPocisk,1);
+						break;
+				case 1: pociskiB |= 2;
+						LCD_zpB(znPocisk,1);
+						break;
+			}
+			
+			strzel = 1;
+			zmstrzel = 3;
+		} else //koniec
+		if (zmstrzel == 5)
+		{
+			strzel = 0;
+			zmstrzel = 0;
+		}
+		
+		//T0 sprawdz przyciski i wyswietl seg7
+		if (sprawdzprz == 1)
+		{
+			sprawdz_przyciski();
+			
+			seg7_wyswietl();
+			
+			sprawdzprz = 0;
+		}
+    }
+}
+
